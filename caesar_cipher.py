4f5638b0ea7b200493b58df72d0ec14aa30c50d4
sarawuwu <sarawu0001@gmail.com>
2022-11-16 15:00:44 +0800

    update caesar

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,10 +5,6 @@ def caesar(start_text, shift_amount, cipher_direction):
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
     if char in alphabet :
       position = alphabet.index(char)
       new_position = position + shift_amount
@@ -17,29 +13,23 @@ def caesar(start_text, shift_amount, cipher_direction):
       end_text+=char
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
 
 import art
 print(art.logo)
+print("-----------------------------------------")
+print(" ")
+print('歡迎來到密碼遊戲，使用這個小程式你可以將你的資料加密或解密，快來試試吧!')
 
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
 should_continue = True
 while should_continue:
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = (int(input("Type the shift number:\n")))%len(alphabet)
+  direction = input("你要加密或解密? 加密輸入: 'encode' 解密輸入: 'decode' \n")
+  text = input("輸入你想要打的文字，請輸入英文、數字或符號:\n").lower()
+  shift = (int(input("你想要順移的單位:\n")))%len(alphabet)
 
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
-#Hint: Think about how you can use the modulus (%).
 
 # new_shift= shift%len(alphabet)
   caesar(start_text=text, shift_amount=shift,     cipher_direction=direction)
-  ans = input("Do you wanna try again? (yes or no)").lower()
+  ans = input("想要再試一次嗎? (yes or no)").lower()
   if ans =="no":
     should_continue = False
     print('Good Bye!!')
